9fb4b981f03d731a5d851e0dfc4fb32a3cfe2aab
suryakc <kiran.cs@gmail.com>
2018-03-16 11:26:23 +0530

    Added Dicey project.

---
+++ Dicey/Dicey/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Dicey
+//
+//  Created by Surya Chundru on 16/03/18.
+//  Copyright © 2018 apolloappstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+}
+
